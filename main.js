627f89347b800426fe64703b05fde11d396582e2
Liam Duggan <dugganl1@tcd.ie>
2025-06-18 13:29:11 +0100

    Initial commit: Audio-reactive 3D wireframe visualizer with Three.js and GLSL shaders

---
+++ main.js
@@ -0,0 +1,68 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Sets the color of the background.
+// renderer.setClearColor(0xfefefe);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Sets orbit control to move the camera around.
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+// Camera positioning.
+camera.position.set(6, 8, 14);
+// Has to be done everytime we update the camera position.
+orbit.update();
+
+const listener = new THREE.AudioListener();
+camera.add(listener);
+
+const sound = new THREE.Audio(listener);
+
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load("/bicep_apricots.mp3", function (buffer) {
+  sound.setBuffer(buffer);
+  window.addEventListener("click", function () {
+    sound.play();
+  });
+});
+
+const analyser = new THREE.AudioAnalyser(sound, 32);
+
+const uniforms = {
+  u_time: { value: 0.0 },
+  u_frequency: { value: 0.0 },
+};
+
+const mat = new THREE.ShaderMaterial({
+  wireframe: true,
+  uniforms,
+  vertexShader: document.getElementById("vertexshader").textContent,
+  fragmentShader: document.getElementById("fragmentshader").textContent,
+});
+
+const geo = new THREE.IcosahedronGeometry(4, 30);
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const clock = new THREE.Clock();
+
+function animate() {
+  uniforms.u_frequency.value = analyser.getAverageFrequency();
+
+  uniforms.u_time.value = clock.getElapsedTime();
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
